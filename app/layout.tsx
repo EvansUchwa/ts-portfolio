ab5560602375847479bbe069054d3b8e7fe249e6
EvansUchwa <johnsonevans686@gmail.com>
2024-07-18 17:54:28 +0100

    Add all meta tags,part 3,use metadata from next js

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,33 @@ import { AsideNav } from "@/uikits/Nav";
 import Footer from "@/uikits/footer";
 import { Toaster } from "sonner";
 import { GoogleTranslate } from "@/pageComponents/others/googleTranslate";
-import Head from "next/head";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "900"],
 });
 
 export const metadata: Metadata = {
-  title: "Evans Djossouvi-Portfolio",
-  description: "DÉVELOPPEUR FULL-STACK JS",
+  title: "Home | Evans Djossouvi",
+  applicationName: "Personal Website - Portfolio",
+  description:
+    "Evans Djossouvi is a Fullstack Developer with expertise in modern web technologies including React.js, Node.js, Next.js, and Capacitor. Specializing in  SaaS applications, Evans creates powerful and scalable solutions for your business needs.",
   icons: "/logo.ico",
+  themeColor: "#9C6E5B",
+  openGraph: {
+    type: "website",
+    url: "https://evansdjossouvi.com/",
+    siteName: "Evans Djossouvi",
+    images: ["/logo.PNG"],
+  },
+  keywords: [
+    "Fullstack Developer",
+    "Web Development",
+    "React.js",
+    "Node.js",
+    "Next.js",
+  ],
+  colorScheme: "light",
 };
 
 export default function RootLayout({
@@ -25,23 +42,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="keywords"
-          content="Evans Djossouvi, Fullstack Developer, Web Development, React.js, Node.js, Next.js"
-        />
-        <meta property="og:site_name" content="Evans Djossouvi" />
-        <meta property="og:url" content="https://evansdjossouvi.com/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Home | Evans Djossouvi" />
-        <meta property="og:image" content="/logo.png" />
-        <meta
-          property="og:description"
-          content="Evans Djossouvi is a Fullstack Developer with expertise in modern web technologies including React.js, Node.js, Next.js, and Capacitor. Specializing in  SaaS applications, Evans creates powerful and scalable solutions for your business needs."
-        />
-        <meta name="msapplication-TileColor" content="#9C6E5B" />
-        <meta name="theme-color" content="#9C6E5B" />
-      </Head>
       <body className={poppins.className}>
         <Toaster richColors={true} />
         <GoogleTranslate />
