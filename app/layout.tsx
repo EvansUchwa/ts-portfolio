f2adbe9804a8400a35414eb7cf42d0ce57d93eea
EvansUchwa <johnsonevans686@gmail.com>
2024-07-18 17:36:41 +0100

    Add all meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { AsideNav } from "@/uikits/Nav";
 import Footer from "@/uikits/footer";
 import { Toaster } from "sonner";
 import { GoogleTranslate } from "@/pageComponents/others/googleTranslate";
+import Head from "next/head";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "900"],
@@ -24,6 +25,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="keywords"
+          content="Evans Djossouvi, Fullstack Developer, Web Development, React.js, Node.js, Next.js"
+        />
+        <meta property="og:site_name" content="Evans Djossouvi" />
+        <meta property="og:url" content="https://evansdjossouvi.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home | Evans Djossouvi" />
+        <meta
+          property="og:image"
+          content="https://evansdjossouvi.com/images/your-image.png"
+        />
+        <meta
+          property="og:description"
+          content="Evans Djossouvi is a Fullstack Developer with expertise in modern web technologies including React.js, Node.js, Next.js, and Capacitor. Specializing in  SaaS applications, Evans creates powerful and scalable solutions for your business needs."
+        />
+        <meta name="msapplication-TileColor" content="#9C6E5B" />
+        <meta name="theme-color" content="#9C6E5B" />
+      </Head>
       <body className={poppins.className}>
         <Toaster richColors={true} />
         <GoogleTranslate />
