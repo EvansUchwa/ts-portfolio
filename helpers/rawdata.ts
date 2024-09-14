f08337a3f02a693aba69f5588677297454513759
EvansUchwa <johnsonevans686@gmail.com>
2024-09-14 19:50:30 +0200

    Update page title

--- helpers/rawdata.ts
+++ helpers/rawdata.ts
@@ -1,121 +1,130 @@
 import { WorkType } from "@/types/others";
 export const works: WorkType[] = [
-    {
-        name: "France-Assist",
-        description:
-            "En tant que développeur chez France-assist, j'ai contribué au développement de France-Assist, une application dédiée à fournir des services d'assistance aux utilisateurs en France. Cette application, qui compte plus de 1500 utilisateurs actifs, offre diverses fonctionnalités pour faciliter l'accès à l'assistance et améliorer l'expérience utilisateur.",
-        img: "franceassist.webp",
-        link: "https://france-assist.fr",
-        technos: ["react", "node", "mongo", "sass"],
-    },
-    {
-        name: "Kloo",
-        description: "En tant que développeur chez All Web Services, j'ai participé à la création et à l'amélioration de Kloo.me, une plateforme polyvalente permettant de créer des micro-pages personnalisables et des liens courts. La plateforme inclut des fonctionnalités avancées telles que le suivi des statistiques, le ciblage des liens, la protection par mot de passe et l'intégration avec divers outils marketing. J'ai travaillé sur le développement de fonctionnalités pour la création de liens WhatsApp, de vCards et de partage de fichiers, ainsi que sur la mise en œuvre de QR codes dynamiques pour une distribution facile du contenu.",
-        img: "kloo.webp",
-        link: "https://kloo.me",
-        technos: ["php", "node", "css", "html", "js"],
-    },
-    {
-        name: "Dofra",
-        description:
-            "Une application web destinée à aider les professionnels du déménagement et du transport de biens en France à trouver des clients. L'application est en cours de lancement et compte déjà une centaine d'entreprises et de clients préinscrits.",
-        img: "dofra.webp",
-        link: "https://dofra.fr",
-        technos: ["next", "mongo", "sass", "gitlab", "docker"],
-    },
-    {
-        name: "Cleopatra",
-        description:
-            "Une plateforme de rencontre pour adultes développée pour un client basé au Canada. Elle compte actuellement 4000 utilisateurs, dont 500 actifs.",
-        img: "cleopatra.webp",
-        link: "https://cleopatra.sx",
-        technos: ["react", "node", "mongo", "sass", "docker"],
-    },
-    {
-        name: "Refi",
-        description:
-            "Une plateforme web développée pour une entreprise immobilière. Elle facilite les interactions entre les clients et l'entreprise du client.",
-        img: "refi.webp",
-        link: "https://refi.bj",
-        technos: ["react", "node", "mongo", "css"],
-    },
-    {
-        name: "VidCensor",
-        description:
-            "C'est un mini SaaS en cours de développement, conçu pour permettre aux YouTubers et à leurs monteurs de censurer automatiquement et efficacement les gros mots dans leurs vidéos grâce à l'utilisation de l'IA.",
-        img: "vidcensor.webp",
-        link: "https://censor-vid.vercel.app/",
-        technos: ["next", "node", "mongo", "vercel", "sass"],
-    },
-    {
-        name: "BNEX",
-        description: "Un site vitrine pour une entreprise de marketing locale. Le site est actuellement en cours de lancement",
-        img: "bnex.webp",
-        link: "https://jose-bnex.vercel.app/",
-        technos: ["next", "ts", "sass", "vercel"],
-    },
-    {
-        name: "Hiretop",
-        description:
-            "Développé en 10 jours pour un test technique, il s'agit d'une application web dédiée à la recherche d'offres d'emploi et de talents digitaux",
-        img: "hiretop.webp",
-        link: "https://hiretop-test.vercel.app/",
-        technos: ["next", "mongo", "vercel"],
-    },
-    {
-        name: "Portfolio",
-        description: "Ce propre portfolio en ligne.",
-        img: "portfolio.webp",
-        link: "https://edsv-portfolio.vercel.app/",
-        technos: ["next", "ts", "vercel"],
-    },
-    {
-        name: "MinanKon Martial Arts",
-        description: "Un site vitrine conçu pour un coach de Taekwondo.",
-        img: "maxCoach.webp",
-        link: "https://max-coach-six.vercel.app/",
-        technos: ["react", "css", "vercel"],
-    },
-    {
-        name: "DidaFire",
-        description:
-            "Un site vitrine créé pour un coach en développement personnel.",
-        img: "didaFire.webp",
-        link: "https://didafir.vercel.app",
-        technos: ["react", "css", "vercel"],
-    },
+  {
+    name: "France-Assist",
+    description:
+      "En tant que développeur chez France-assist, j'ai contribué au développement de France-Assist, une application dédiée à fournir des services d'assistance aux utilisateurs en France. Cette application, qui compte plus de 1500 utilisateurs actifs, offre diverses fonctionnalités pour faciliter l'accès à l'assistance et améliorer l'expérience utilisateur.",
+    img: "franceassist.webp",
+    link: "https://france-assist.fr",
+    technos: ["react", "node", "mongo", "sass"],
+  },
+  {
+    name: "Kloo",
+    description:
+      "En tant que développeur chez All Web Services, j'ai participé à la création et à l'amélioration de Kloo.me, une plateforme polyvalente permettant de créer des micro-pages personnalisables et des liens courts. La plateforme inclut des fonctionnalités avancées telles que le suivi des statistiques, le ciblage des liens, la protection par mot de passe et l'intégration avec divers outils marketing. J'ai travaillé sur le développement de fonctionnalités pour la création de liens WhatsApp, de vCards et de partage de fichiers, ainsi que sur la mise en œuvre de QR codes dynamiques pour une distribution facile du contenu.",
+    img: "kloo.webp",
+    link: "https://kloo.me",
+    technos: ["php", "node", "css", "html", "js"],
+  },
+  {
+    name: "Dofra",
+    description:
+      "Une application web destinée à aider les professionnels du déménagement et du transport de biens en France à trouver des clients. L'application est en cours de lancement et compte déjà une centaine d'entreprises et de clients préinscrits.",
+    img: "dofra.webp",
+    link: "https://dofra.fr",
+    technos: ["next", "mongo", "sass", "gitlab", "docker"],
+  },
+  {
+    name: "CMPE Commodity",
+    description:
+      "Une application web destinée à aider les professionnels du déménagement et du transport de biens en France à trouver des clients. L'application est en cours de lancement et compte déjà une centaine d'entreprises et de clients préinscrits.",
+    img: "cmpe.webp",
+    link: "https://dofra.fr",
+    technos: ["next", "sass", "github"],
+  },
+  {
+    name: "Cleopatra",
+    description:
+      "Une plateforme de rencontre pour adultes développée pour un client basé au Canada. Elle compte actuellement 4000 utilisateurs, dont 500 actifs.",
+    img: "cleopatra.webp",
+    link: "https://cleopatra.sx",
+    technos: ["react", "node", "mongo", "sass", "docker"],
+  },
+  {
+    name: "Refi",
+    description:
+      "Une plateforme web développée pour une entreprise immobilière. Elle facilite les interactions entre les clients et l'entreprise du client.",
+    img: "refi.webp",
+    link: "https://refi.bj",
+    technos: ["react", "node", "mongo", "css"],
+  },
+  {
+    name: "VidCensor",
+    description:
+      "C'est un mini SaaS en cours de développement, conçu pour permettre aux YouTubers et à leurs monteurs de censurer automatiquement et efficacement les gros mots dans leurs vidéos grâce à l'utilisation de l'IA.",
+    img: "vidcensor.webp",
+    link: "https://censor-vid.vercel.app/",
+    technos: ["next", "node", "mongo", "vercel", "sass"],
+  },
+  {
+    name: "BNEX",
+    description:
+      "Un site vitrine pour une entreprise de marketing locale. Le site est actuellement en cours de lancement",
+    img: "bnex.webp",
+    link: "https://jose-bnex.vercel.app/",
+    technos: ["next", "ts", "sass", "vercel"],
+  },
+  {
+    name: "Hiretop",
+    description:
+      "Développé en 10 jours pour un test technique, il s'agit d'une application web dédiée à la recherche d'offres d'emploi et de talents digitaux",
+    img: "hiretop.webp",
+    link: "https://hiretop-test.vercel.app/",
+    technos: ["next", "mongo", "vercel"],
+  },
+  {
+    name: "Portfolio",
+    description: "Ce propre portfolio en ligne.",
+    img: "portfolio.webp",
+    link: "https://edsv-portfolio.vercel.app/",
+    technos: ["next", "ts", "vercel"],
+  },
+  {
+    name: "MinanKon Martial Arts",
+    description: "Un site vitrine conçu pour un coach de Taekwondo.",
+    img: "maxCoach.webp",
+    link: "https://max-coach-six.vercel.app/",
+    technos: ["react", "css", "vercel"],
+  },
+  {
+    name: "DidaFire",
+    description:
+      "Un site vitrine créé pour un coach en développement personnel.",
+    img: "didaFire.webp",
+    link: "https://didafir.vercel.app",
+    technos: ["react", "css", "vercel"],
+  },
 
-    {
-        name: "Tags-concept",
-        description:
-            "Un site vitrine réalisé pour un client basé aux États-Unis.",
-        img: "tagConcept.webp",
-        link: "https://christian-startup-vitrine.vercel.app/",
-        technos: ["react", "css", "vercel"],
-    },
+  {
+    name: "Tags-concept",
+    description: "Un site vitrine réalisé pour un client basé aux États-Unis.",
+    img: "tagConcept.webp",
+    link: "https://christian-startup-vitrine.vercel.app/",
+    technos: ["react", "css", "vercel"],
+  },
 
-    {
-        name: "Inperfect",
-        description:
-            "Une reproduction avec animations d'un site vitrine Webflow, réalisée pour le plaisir.",
-        img: "inperfect.webp",
-        link: "https://inperfect-test.vercel.app/",
-        technos: ["html", "css", "vercel"],
-    },
-    {
-        name: "Card Design Simulator",
-        description:
-            "Un prototype de simulateur en ligne permettant de personnaliser des cartes de visite.",
-        img: "cardSimulator.webp",
-        link: "https://kloo-card-simulator.vercel.app/",
-        technos: ["react", "css", "vercel"],
-    },
-    {
-        name: "Todo List App",
-        description:
-            "Mon tout premier projet, une application de gestion de tâches basique.",
-        img: "s1.webp",
-        link: "https://to-do-app-react-snowy.vercel.app/",
-        technos: ["react", "css", "vercel"],
-    },
+  {
+    name: "Inperfect",
+    description:
+      "Une reproduction avec animations d'un site vitrine Webflow, réalisée pour le plaisir.",
+    img: "inperfect.webp",
+    link: "https://inperfect-test.vercel.app/",
+    technos: ["html", "css", "vercel"],
+  },
+  {
+    name: "Card Design Simulator",
+    description:
+      "Un prototype de simulateur en ligne permettant de personnaliser des cartes de visite.",
+    img: "cardSimulator.webp",
+    link: "https://kloo-card-simulator.vercel.app/",
+    technos: ["react", "css", "vercel"],
+  },
+  {
+    name: "Todo List App",
+    description:
+      "Mon tout premier projet, une application de gestion de tâches basique.",
+    img: "s1.webp",
+    link: "https://to-do-app-react-snowy.vercel.app/",
+    technos: ["react", "css", "vercel"],
+  },
 ];
